1de13c4fc1a36287dd194e85a1b41ee0989e130f
lopes-pk <arthurlopes558@gmail.com>
2023-07-22 14:23:28 -0300

    added new project server side

---
+++ explorer/API/src/routes/user.routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express')
+
+const usersRoutes = Router()
+
+usersRoutes.post('/', (req, res) => {
+    const { name, email, password } = req.body
+    res.json({ name, email, password })
+});
